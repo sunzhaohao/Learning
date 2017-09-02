0caa440aa4805a83351ab8f1401188f004a34276
sunzhaohao <sunzhaohenan@yahoo.com>
2017-09-02 22:05:28 +0800

    LoL，login screen

---
+++ LOLGame/Assets/scripts/ScreenProcess.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class ScreenProcess : MonoBehaviour {
+
+    [SerializeField]
+    private InputField account;
+    [SerializeField]
+    private InputField password;
+
+    // Use this for initialization
+    void Start() {
+
+    }
+
+    public void loginOnClick()
+    {
+        if (account.text == string.Empty || password.text == string.Empty)
+            return;
+        if (account.text == "sunzhao" &&  password.text=="sunzhao")
+            Debug.Log("login in");
+    }
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}

65b50fe5215012ad2e922fd0d0670335db1dbaef
sunzhaohao <sunzhaohenan@yahoo.com>
2017-09-11 23:06:02 +0800

    LoL 3-3
    register Panel  is completed

--- LOLGame/Assets/scripts/ScreenProcess.cs
+++ LOLGame/Assets/scripts/ScreenProcess.cs
@@ -10,19 +10,62 @@ public class ScreenProcess : MonoBehaviour {
     [SerializeField]
     private InputField password;
 
+    [SerializeField]
+    private Button loginBtn;
     // Use this for initialization
-    void Start() {
 
+    [SerializeField]
+    private GameObject registerPanel;
+
+    [SerializeField]
+    private InputField regAccount;
+    [SerializeField]
+    private InputField regPassword;
+    [SerializeField]
+    private InputField repeatPass;
+
+    void Start() {
+        registerPanel.active = false;
     }
 
+    // login  in 
     public void loginOnClick()
     {
         if (account.text == string.Empty || password.text == string.Empty)
             return;
         if (account.text == "sunzhao" &&  password.text=="sunzhao")
             Debug.Log("login in");
+        // loginBtn.enabled = false;
+        loginBtn.interactable = false;
     }
 	
+    //show  register account  panel
+    public void registerOnClick()
+    {
+        registerPanel.active = true;
+    }
+    // close register panel
+    public void closeRegisterPanelOnClick()
+    {
+        registerPanel.SetActive(false);
+    }
+    // register account
+    public void registerAccOnClick()
+    {
+        if(regAccount.text==string.Empty || regPassword.text == string.Empty)
+        {
+            Debug.Log("can not be null ！");
+            return;
+        }
+        if (regPassword.text != repeatPass.text)
+        {
+            Debug.Log("two passwords are different!");
+            return;
+        }
+        Debug.Log("registed successfully");
+        registerPanel.active = false;
+    }
+
 	// Update is called once per frame
 	void Update () {
 		

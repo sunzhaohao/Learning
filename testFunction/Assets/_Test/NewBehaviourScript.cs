9b4a0e292f0c03c9ae47e1cd6abef02cc9bf74a0
sunzhaohao <sunzhaohenan@yahoo.com>
2017-09-25 23:49:16 +0800

    plugin

---
+++ testFunction/Assets/_Test/NewBehaviourScript.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UniRx.Triggers;
+
+public class NewBehaviourScript : MonoBehaviour {
+
+    public GameObject obj;
+
+	// Use this for initialization
+	void Start () {
+		obj.OnTriggerEnterAsObservable()
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}

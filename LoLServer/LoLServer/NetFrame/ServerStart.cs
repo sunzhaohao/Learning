3e69ccf48f30f5cbfcda50cee413c55ae054fd37
sunzhaohao <sunzhaohenan@yahoo.com>
2017-09-25 20:31:57 +0800

    LoL Server

---
+++ LoLServer/LoLServer/NetFrame/ServerStart.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Net.Sockets;
+namespace NetFrame
+{
+
+    public class ServerStart
+    {
+        Socket server; // 服务器监听对象
+        int macClient; // 最大客户连接数目
+        public ServerStart(int max,int port)
+        {
+            server = new Socket(AddressFamily.InterNetwork, SocketType.Stream,ProtocolType.Tcp);
+            this.macClient = max;
+        }
+    }
+}

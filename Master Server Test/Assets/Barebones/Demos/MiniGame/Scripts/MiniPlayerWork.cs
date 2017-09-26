abec652ae11f99c441cf6d74b967b27b7921f831
sunzhaohao <sunzhaohenan@yahoo.com>
2017-09-26 22:13:56 +0800

    master-server-framework
    2017-9-26

---
+++ Master Server Test/Assets/Barebones/Demos/MiniGame/Scripts/MiniPlayerWork.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Networking;
+
+public class MiniPlayerWork : NetworkBehaviour
+{
+
+    public Transform tran;
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update ()
+	{
+        // ignore other player's input
+	    if (!isLocalPlayer)
+	        return;
+	    if (Input.GetKeyDown(KeyCode.Space))
+	    {
+	        tran.position=new Vector3(tran.position.x,tran.position.y+10,tran.position.z);
+	    }
+	}
+}
---
+++ Master Server Test/Assets/readme.txt
@@ -0,0 +1,4 @@
+2017-9-26
+游戏场景：SimplePlatform 
+玩家预设：MiniPlayer
+编写脚本：MiniPlayerWork.cs 
